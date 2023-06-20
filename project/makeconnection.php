214a452ce647b2ccb77b9768971a77d3bd928d83
Ajit Gavade <94741808+thecamron@users.noreply.github.com>
2023-06-20 11:58:38 +0530

    Delete project directory

--- project/makeconnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername="localhost";
-		$username="root";
-		$password="Sabir_123";
-		$dbnm="db1";
-		$con=new mysqli($servername,$username,$password,$dbnm);
-?>
--- project/user/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-//for logout
-session_start();
-session_unset();
-session_destroy();
-header("Location:/project/login.php");
-?>
